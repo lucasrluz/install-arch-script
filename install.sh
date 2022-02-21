f63597be780bd246a33e5f2be6c08958e45bad24
lucasrluz <lucasr.luzbr@gmail.com>
2022-02-21 20:26:35 -0300

    montando sistema de arquivos

--- install.sh
+++ install.sh
@@ -7,3 +7,17 @@ mkfs.ext4 /dev/sda2
 mkfs.ext4 /dev/sda3
 
 mkswap /dev/sda4
+
+mount /dev/sda2 /mnt
+
+mkdir /mnt/home
+
+mkdir /mnt/boot
+
+mkdir /mnt/boot/efi
+
+mount /dev/sda3 /mnt/home
+
+mount /dev/sda1 /mnt/boot/efi
+
+swapon /dev/sda4
