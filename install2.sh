ae4cba0895066142bad1ca35faf404914f825642
lucasrluz <lucasr.luzbr@gmail.com>
2022-02-21 20:46:44 -0300

    configuração dos hosts

--- install2.sh
+++ install2.sh
@@ -9,3 +9,9 @@ echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
 echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
 
 echo "arch" > /etc/hostname
+
+echo "127.0.0.1 localhost.localdomain localhost" > /etc/hosts
+
+echo "::1 localhost.localdomain localhost" >> /etc/hosts
+
+echo "127.0.1.1	arch.localdomain arch" >> /etc/hosts
