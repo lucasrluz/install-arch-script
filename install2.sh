2761676b2d4f454cfae165257dd6d0232dee3784
lucasrluz <lucasr.luzbr@gmail.com>
2022-02-21 20:45:25 -0300

    configurações de idioma

--- install2.sh
+++ install2.sh
@@ -1,1 +1,9 @@
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+
+echo pt_BR.UTF-8 UTF-8 >> /etc/locale.gen
+
+locale-gen
+
+echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
+
+echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
