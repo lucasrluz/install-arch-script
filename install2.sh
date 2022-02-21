1f6269d8306a79274ce6860f722523e4b95a24d6
lucasrluz <lucasr.luzbr@gmail.com>
2022-02-21 20:48:36 -0300

    instalação do NetworkManager

--- install2.sh
+++ install2.sh
@@ -21,3 +21,7 @@ pacman -S grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 
 grub-mkconfig -o /boot/grub/grub.cfg
+
+pacman -S networkmanager
+
+systemctl enable NetworkManager
