aa861c00ca2038f2ca0196efdbcd36cbcb712532
lucasrluz <lucasr.luzbr@gmail.com>
2022-02-22 22:49:57 -0300

    Adicionado comentários para cada parte dos scripts

--- install.sh
+++ install.sh
@@ -1,31 +1,29 @@
+# Set br-abnt2 keyboard layout
 loadkeys br-abnt2
 
+# Create the file systems for the new partitions 
 mkfs.fat -F32 /dev/sda1
-
 mkfs.ext4 /dev/sda2
-
 mkfs.ext4 /dev/sda3
-
 mkswap /dev/sda4
 
+# Mount the newly created partitons
 mount /dev/sda2 /mnt
-
 mkdir /mnt/home
-
 mkdir /mnt/boot
-
 mkdir /mnt/boot/efi
-
 mount /dev/sda3 /mnt/home
-
 mount /dev/sda1 /mnt/boot/efi
-
 swapon /dev/sda4
 
+# Install the base system
 pacstrap /mnt base linux linux-firmware
 
+# Automatically generate the fstab file from the mount configuration
 genfstab -U /mnt >> /mnt/etc/fstab
 
+# Copy the second file for the advanced config process to the hdd
 cp ./install2.sh /mnt
 
+# Switch to the newly installed system and run the second file
 arch-chroot /mnt /install2.sh
--- install2.sh
+++ install2.sh
@@ -1,33 +1,34 @@
+# Set the timezone to São Paulo
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 
+# Set the locale
 echo pt_BR.UTF-8 UTF-8 >> /etc/locale.gen
-
 locale-gen
-
 echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
-
 echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
 
+# Set a new hostname
 echo "arch" > /etc/hostname
 
+# Set hosts
 echo "127.0.0.1 localhost.localdomain localhost" > /etc/hosts
-
 echo "::1 localhost.localdomain localhost" >> /etc/hosts
-
 echo "127.0.1.1	arch.localdomain arch" >> /etc/hosts
 
+# Install and configure grub
 pacman -S grub efibootmgr
-
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-
 grub-mkconfig -o /boot/grub/grub.cfg
 
+# Install and enable networkmanager
 pacman -S networkmanager
-
 systemctl enable NetworkManager
 
+# Set root password
 passwd
 
+# Create new user
 useradd -m -g users -G wheel lucasluz
 
+# Set the new user password
 passwd lucasluz
