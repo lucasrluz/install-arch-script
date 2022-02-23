562a00a4768916f255b88cf316a4f9061ee4327a
lucasrluz <lucasr.luzbr@gmail.com>
2022-02-23 17:24:45 -0300

    adicionado font Inter para instalação

--- install-part-3.sh
+++ install-part-3.sh
@@ -1,5 +1,5 @@
 # Install package for personal use
-sudo pacman -Syu base-devel xorg xorg-xinit pulseaudio pavucontrol ttf-jetbrains-mono wget chromium thunar thunar-volman gvfs lxappearance git zsh neovim i3-wm rofi alacritty feh
+sudo pacman -Syu base-devel xorg xorg-xinit pulseaudio pavucontrol inter-font ttf-jetbrains-mono wget chromium thunar thunar-volman gvfs lxappearance git zsh neovim i3-wm rofi alacritty feh
 
 # Install yay
 git clone https://aur.archlinux.org/yay.git
