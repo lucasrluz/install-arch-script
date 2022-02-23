0891757e1799b12bff458ef4898c4a431f3f57f4
lucasrluz <lucasr.luzbr@gmail.com>
2022-02-23 14:38:16 -0300

    instalação e configuração do zsh

--- install-part-3.sh
+++ install-part-3.sh
@@ -8,3 +8,13 @@ makepkg -si
 
 # Install package for personal use
 yay -S vscodium-bin spotify beekeeper-studio-bin
+
+# Install oh my zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install and configure oh my zsh theme
+git clone https://github.com/reobin/typewritten.git $ZSH_CUSTOM/themes/typewritten
+ln -s "$ZSH_CUSTOM/themes/typewritten/typewritten.zsh-theme" "$ZSH_CUSTOM/themes/typewritten.zsh-theme"
+ln -s "$ZSH_CUSTOM/themes/typewritten/async.zsh" "$ZSH_CUSTOM/themes/async"
+
+echo "Set ZSH_THEME="typewritten" in your .zshrc file."
